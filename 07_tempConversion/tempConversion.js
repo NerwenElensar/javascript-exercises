3a675d88cd356f15834f1a1931587a299514e610
Nerwen <jenny2209@gmx.de>
2025-01-20 15:19:10 +0100

    solve exercises 1-7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (tempInF) {
+  //°C = (°F − 32) × 5/9
+  return Number.parseFloat(((tempInF - 32) * (5 / 9)).toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (tempInC) {
+  //(Temperature in degrees Celsius (°C) * 9/5) + 32.
+  return Number.parseFloat((tempInC * (9 / 5) + 32).toFixed(1));
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
